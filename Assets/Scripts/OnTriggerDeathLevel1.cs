0b3cc9519c61c1348197c39c73030c3e823b1bfb
jackahlberg <ahlberg.jack@gmail.com>
2022-02-11 08:36:11 +0100

    .

--- Assets/Scripts/OnTriggerDeathLevel1.cs
+++ Assets/Scripts/OnTriggerDeathLevel1.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnTriggerDeathLevel1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Level1");
+        }
     }
 }
